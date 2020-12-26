5f9a9b6796fc8534ca6389e07d15aed7d37c9780
Adam Kogut <adamkgt@gmail.com>
2020-12-26 21:36:55 +0100

    #4 Rozwiązanie zadania2.

---
+++ src/TikTak.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+
+public class TikTak {
+    public static void main(String[] args) {
+
+        int i=427;
+        while (i<=529)
+        {
+
+            if (i%15==0)
+            {
+                System.out.println("TikTak");
+            }
+
+            else if (i % 5 == 0)
+            {
+                System.out.println("Tak");
+            }
+            else if (i % 3 == 0)
+            {
+                System.out.println("Tik");
+            }
+            i++;
+                }
+
+
+
+            }
+        }
+
